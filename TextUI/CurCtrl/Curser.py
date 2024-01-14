cabcef1f978e79858c9d1162d6a42435b03fe09b
warm20 <101261620+warm20@users.noreply.github.com>
2024-01-14 03:45:28 +0000

    update some file

---
+++ TextUI/CurCtrl/Curser.py
@@ -0,0 +1,12 @@
+class Curser:
+    def MoveTo(self, x:int, y:int):
+        print("/33[%d;%d"%(y,x), end="")
+    def Clear(self):
+        print("\33[2J", end="")
+    def Hide(self):
+        print("\33[?25l", end="")
+    def Show(self):
+        print("\33[?25h", end="")
+    def ClearBuff(self):
+        print("\33[0m" ,end="")
+    
---
+++ TextUI/dom/element.py
@@ -0,0 +1,7 @@
+class Element:
+    # pos(x,y) is the left and top
+    # the element draw view will from pos(x,y) to (x+w,y+h)
+    def __init__(self,x,y) -> None:
+        self.x = x
+        self.y = y
+        pass
